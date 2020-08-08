e491c0b729f75c96c231ed0b15609ba26c0b7060
Adiimated <adiimated@gmail.com>
2020-08-08 07:38:09 -0700

    Create Iris_using_kmeans.R

---
+++ Task # 3 - To Explore Unsupervised Machine Learning/Iris_using_kmeans.R
@@ -0,0 +1,35 @@
+# K-Means Clustering
+
+# Importing the dataset
+dataset = read.csv('Iris.csv')
+dataset = dataset[2:3]
+
+# Using the elbow method to find the optimal number of clusters
+set.seed(6)
+wcss = vector()
+for (i in 1:10) wcss[i] = sum(kmeans(dataset, i)$withinss)
+plot(1:10,
+     wcss,
+     type = 'b',
+     main = paste('The Elbow Method'),
+     xlab = 'Number of clusters',
+     ylab = 'WCSS')
+
+# Fitting K-Means to the dataset
+set.seed(29)
+kmeans = kmeans(x = dataset, centers = 3)
+y_kmeans = kmeans$cluster
+
+# Visualising the clusters
+library(cluster)
+clusplot(dataset,
+         y_kmeans,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Clusters of Sepals'),
+         xlab = 'Sepal Length',
+         ylab = 'Sepal Width')
