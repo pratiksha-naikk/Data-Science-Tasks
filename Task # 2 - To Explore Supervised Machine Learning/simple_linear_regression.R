47ce106c2e4cd6b2aafa11ca76d9e6cbbf31c82c
Adiimated <adiimated@gmail.com>
2020-08-11 02:09:18 -0700

    Create simple_linear_regression.R

---
+++ Task # 2 - To Explore Supervised Machine Learning/simple_linear_regression.R
@@ -0,0 +1,41 @@
+# Exploring Supervised Machine Learning
+
+# Importing the dataset
+dataset = read.csv('hours.csv')
+
+# Splitting the dataset into training data and test data
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Scores, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Fitting Simple Linear Regression to the Training set
+regressor = lm(formula = Scores ~ Hours,
+               data = training_set)
+
+# Predicting the Test set results
+y_pred = predict(regressor, newdata = test_set)
+
+# Visualising the Training set results
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = training_set$Hours, y = training_set$Scores),
+             colour = 'red') +
+  geom_line(aes(x = training_set$Hours, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Scores vs Hours (Training set)') +
+  xlab('Hours') +
+  ylab('Scores')
+
+# Visualising the Test set results
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = test_set$Hours, y = test_set$Scores),
+             colour = 'red') +
+  geom_line(aes(x = training_set$Hours, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Scores vs Hours (Test set)') +
+  xlab('Hours') +
+  ylab('Scores')
