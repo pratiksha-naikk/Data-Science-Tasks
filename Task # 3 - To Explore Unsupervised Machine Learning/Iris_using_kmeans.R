6f70b20a1a5baedb51df553da882541bd3daffe1
Adiimated <adiimated@gmail.com>
2020-08-13 09:47:24 -0700

    Update Iris_using_kmeans.R

--- Task # 3 - To Explore Unsupervised Machine Learning/Iris_using_kmeans.R
+++ Task # 3 - To Explore Unsupervised Machine Learning/Iris_using_kmeans.R
@@ -1,35 +1,30 @@
-# K-Means Clustering
+# Task # 3 - To Explore Unsupervised Machine Learning
 
-# Importing the dataset
-dataset = read.csv('Iris.csv')
-dataset = dataset[2:3]
+# Loading the Libraries
+library(ggplot2)
+theme_set(theme_bw(base_size=12)) # set default ggplot2 theme
+library(dplyr)
 
-# Using the elbow method to find the optimal number of clusters
-set.seed(6)
-wcss = vector()
-for (i in 1:10) wcss[i] = sum(kmeans(dataset, i)$withinss)
-plot(1:10,
-     wcss,
-     type = 'b',
-     main = paste('The Elbow Method'),
-     xlab = 'Number of clusters',
-     ylab = 'WCSS')
+# Elbow Method To Determine Optimum Number of Clusters
+set.seed(200)
+k.max <- 10
+wss<- sapply(1:k.max,function(k){kmeans(iris[,3:4],k,nstart = 20,iter.max = 20)$tot.withinss})
+wss
 
-# Fitting K-Means to the dataset
-set.seed(29)
-kmeans = kmeans(x = dataset, centers = 3)
-y_kmeans = kmeans$cluster
+plot(1:k.max,wss, type= "b", xlab = "Number of clusters(k)", ylab = "Within cluster sum of squares")
 
-# Visualising the clusters
-library(cluster)
-clusplot(dataset,
-         y_kmeans,
-         lines = 0,
-         shade = TRUE,
-         color = TRUE,
-         labels = 2,
-         plotchar = FALSE,
-         span = TRUE,
-         main = paste('Clusters of Sepals'),
-         xlab = 'Sepal Length',
-         ylab = 'Sepal Width')
+
+# Removing the unnecessary column and analysing the dataset
+iris %>% select(-Species) %>% 
+  kmeans(centers=3) ->      
+  km                          
+
+# Reviewing the results
+km
+km$centers
+
+# Visualizing the clusters
+iris %>% select(-Species) %>% kmeans(centers=3, nstart=10) -> km
+iris_clustered <- data.frame(iris, cluster=factor(km$cluster))
+ggplot(iris_clustered, aes(x=Petal.Width, y=Sepal.Width, color=cluster, shape=Species)) + geom_point()
+
